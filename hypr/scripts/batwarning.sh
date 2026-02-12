c30d277c0de2c1a2b54cb7edd7d1cab1ad5c8ad5
Devious-pumpkin <lord-leviathan@tuta.com>
2026-02-12 17:35:14 +0100

    added a sound to the batwarning and waybar displays the weekday

--- hypr/scripts/batwarning.sh
+++ hypr/scripts/batwarning.sh
@@ -7,14 +7,17 @@ while [ true ]; do
     below_count=$((below_count + 1))
     #echo "it's $below_count"
     if [ $below_count == 1 ]; then
+      play ~/.config/hypr/sounds/alert.mp3
       dunstify -u critical "BATTERY LOW PLEASE CHARGE"
     fi
   elif [ $(upower -d | grep percentage | grep -o -m 1 -E '[0-9]+') -le 4 ]; then
     dunstify -u critical "POWERING OFF"
+    play ~/.config/hypr/sounds/alert.mp3
     sleep 10s
     poweroff
   else
     below_count=0
+    #play ~/.config/hypr/sounds/alert.mp3
     #echo "it's $below_count"
   fi
   sleep 60s
